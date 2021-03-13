20aac113ac025e73d0d80cd2555bb6430b92cd1d
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-13 16:31:28 +0000

    Knight class added

--- knights_travails.rb
+++ knights_travails.rb
@@ -5,3 +5,11 @@ class Node
     @right = nil
   end
 end
+
+class Knight
+  attr_reader :moves
+
+  def initialize
+    @moves = [[-2, -1], [-1, -2], [-2, 1], [-1, 2], [1, -2], [2, -1], [1, 2], [2, 1]]
+  end
+end
